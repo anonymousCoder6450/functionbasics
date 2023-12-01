c75512f505728005878658fe2e818504dc6d46f6
camcam123 <133077837+anonymousCoder6450@users.noreply.github.com>
2023-12-01 18:57:58 -0500

    functions.py

---
+++ functions.py
@@ -0,0 +1,21 @@
+#Welcome to the first example, functions have 3 distinct things that need to be filled in, Our name, parameters and arguments. 
+
+def func(x):
+  return x + 5
+#So in this function it's quite small, functions are able to be reused for other pieces of code and I like to think of it like a black box
+#Where you have an input, the process and then the output. For this function we start of with a "def" this tells our IDE/text editor that 
+# we're defining a new function (hence the name def), we then have our name which is orange which can be ANYTHING, I recommend you make it lowercase
+#as it's easier and it won't error when creating VERY big programs as well as not to nate it "range" "input" or any name that's already a function
+#in python, the IDE will mix both up at one point and your code will eventually mix up and you will get mixed up
+
+#Now moving on, our return in the function is called a return statement, in functions 99.9% of the time you'll have a return statement that actually returns something
+#if you don't include a return statement it'll just print "None" in the terminal
+#Here try this code out and see
+def func1(x)
+  if x == 6
+  return "Hello World"
+
+# you notice how you dont need to include the brackets liek you do for the print function which makes it way more clean in your code.
+
+#In summary functions are blocks of code that can be used anywhere in your code as many times as possible, 
+#This is a very simplified explanation of function and I will make a more complex one in the future
